f6b8b150ba622191ae1c92799c4e659a881e02ba
SaikatOp <141167640+SaikatOp@users.noreply.github.com>
2025-05-21 17:44:41 +0530

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,41 @@
+package com.example.todo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private boolean completed = false;
+
+    // Constructors
+    public Task() {}
+
+    public Task(String title) {
+        this.title = title;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
